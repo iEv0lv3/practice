0f5b2f1387a600ace2f8de061a1b463fb2868910
Jordan <jordan.wallace.williams@gmail.com>
2020-07-16 21:30:26 -0600

    Interview Cake: Merging Ranges

--- interview_cake/merging_ranges.rb
+++ interview_cake/merging_ranges.rb
@@ -1,2 +1,16 @@
-require 'pry'
-require 'rspec'
+def find_time(meetings)
+  sorted = meetings.sort
+  iteration = 0
+
+  sorted.each do |meeting|
+    if meeting[1] >= sorted[1 + iteration][0] && meeting[1] <= sorted[1 + iteration][1]
+      sorted[1 + iteration] = [meeting[0], sorted[1 + iteration][1]]
+      sorted.delete_at(iteration)
+    elsif meeting[0] <= sorted[1 + iteration][0] && meeting[1] >= sorted[1 + iteration][1]
+      sorted.delete_at(1 + iteration)
+    end
+    iteration += 1
+  end
+
+  sorted
+end
--- interview_cake/spec/merging_ranges_spec.rb
+++ interview_cake/spec/merging_ranges_spec.rb
@@ -1,8 +1,28 @@
 require 'pry'
 require './merging_ranges'
 
-describe 'When I do something' do
-  it 'it has this outcome' do
+describe 'Merging Ranges' do
+  it 'can sort a nested array of meetings' do
+    meetings = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
 
+    expect(find_time(meetings)).to eq([[0, 1], [3, 8], [9, 12]])
+  end
+
+  it 'can handle a meeting that is completely within another meeting time block' do
+    meetings = [[1, 5], [2, 3]]
+
+    expect(find_time(meetings)).to eq([[1, 5]])
+  end
+
+  it 'can handle consecutive meetings that end and start at the same time' do
+    meetings = [[1, 2], [2, 3]]
+
+    expect(find_time(meetings)).to eq([[1, 3]])
+  end
+
+  it 'can handle merging all meetings into one array' do
+    meetings = [[1, 10], [2, 6], [3, 5], [7, 9]]
+
+    expect(find_time(meetings)).to eq([[1, 10]])
   end
 end
