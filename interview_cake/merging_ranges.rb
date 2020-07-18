951b8cbc4e56b08d9d5552c95f7b6cc95c2ac103
Jordan <jordan.wallace.williams@gmail.com>
2020-07-18 14:51:15 -0600

    Add test for edge case: all meeting blocks fall within a single meeting block

--- interview_cake/merging_ranges.rb
+++ interview_cake/merging_ranges.rb
@@ -3,14 +3,33 @@ def find_time(meetings)
   iteration = 0
 
   sorted.each do |meeting|
-    if meeting[1] >= sorted[1 + iteration][0] && meeting[1] <= sorted[1 + iteration][1]
+    require 'pry'; binding.pry
+    if sorted[0][0] <= meeting[0] && sorted[0][1] >= meeting[1]
+      require 'pry'; binding.pry
+      # sorted[0] = meeting
+      sorted.delete_at(1)
+    elsif meeting[1] >= sorted[1 + iteration][0] && meeting[1] <= sorted[1 + iteration][1]
+      require 'pry'; binding.pry
       sorted[1 + iteration] = [meeting[0], sorted[1 + iteration][1]]
       sorted.delete_at(iteration)
-    elsif meeting[0] <= sorted[1 + iteration][0] && meeting[1] >= sorted[1 + iteration][1]
-      sorted.delete_at(1 + iteration)
+      iteration += 1
     end
-    iteration += 1
+    require 'pry'; binding.pry
   end
+  require 'pry'; binding.pry
+
+  # sorted.size.times do
+  #   meeting = sorted[0 + iteration]
+  #   if sorted[0][0] <= meeting[0] && sorted[0][1] >= meeting[1]
+  #     deleted += 1
+  #     iteration += 1
+  #     sorted.delete_at(1)
+  #   elsif meeting[1] >= sorted[1 + iteration][0] && meeting[1] <= sorted[1 + iteration][1]
+  #     sorted[1 + iteration] = [meeting[0], sorted[1 + iteration][1]]
+  #     sorted.delete_at(iteration)
+  #     iteration += 1
+  #   end
+  # end
 
   sorted
 end
