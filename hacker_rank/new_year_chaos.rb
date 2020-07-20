abb79815f54d95af336deec6db575c173f3a0b84
Jordan <jordan.wallace.williams@gmail.com>
2020-07-19 21:02:27 -0600

    Add control flow for checking only line[index +2] <= 2

--- hacker_rank/new_year_chaos.rb
+++ hacker_rank/new_year_chaos.rb
@@ -14,5 +14,16 @@ def minimum_bribes(line)
       # (check that line[index] value - line[index + 1] value).abs <= 1
       # (check that line[index] value - line[index - 2] value).abs <= 2
     end
+
+    # Try a control flow that only checks list[index + 2] <= index
+    line.each_index do |index|
+      # (check that line[0] value - line[index + 2] value).abs <= 2
+      if (line[index] - line[index + 2]).abs <= 2
+        next
+      else
+        'Too Complex'
+      end
+
+    end
   end
 end
